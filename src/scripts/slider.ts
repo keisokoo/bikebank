dfd7476178d0893f0b76a9892eb8d33529f0142e
keisokoo <keisokoo@doubled.kr>
2022-06-13 11:46:28 +0900

    add, scooter page

--- src/scripts/slider.ts
+++ src/scripts/slider.ts
@@ -1,10 +1,26 @@
 import Swiper from "swiper"
 
 document.addEventListener("DOMContentLoaded", () => {
-  const swiper = new Swiper(".swiper", {
+  const swiper = new Swiper(".swiper-motors", {
     // Optional parameters
-    direction: "vertical",
-    loop: true,
+    direction: "horizontal",
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: false,
+    breakpoints: {
+      300: {
+        slidesPerView: 3,
+        spaceBetween: 8,
+      },
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      1048: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+    },
   })
   console.log(swiper)
 })
