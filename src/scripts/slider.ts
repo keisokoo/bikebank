29ec7578f3acb37868809032f0866159546bc99a
keisokoo <keisokoo@doubled.kr>
2022-06-13 14:39:02 +0900

    add, scooter sliders

--- src/scripts/slider.ts
+++ src/scripts/slider.ts
@@ -1,4 +1,4 @@
-import Swiper from "swiper"
+import Swiper, { Navigation } from "swiper"
 
 document.addEventListener("DOMContentLoaded", () => {
   const swiper = new Swiper(".swiper-motors", {
@@ -19,6 +19,77 @@ document.addEventListener("DOMContentLoaded", () => {
       1048: {
         slidesPerView: 4,
         spaceBetween: 30,
+      },
+    },
+  })
+  new Swiper(".swiper-describe-one", {
+    // Optional parameters
+    direction: "horizontal",
+    modules: [Navigation],
+    navigation: {
+      nextEl: ".swiper-describe-one>.slide-control-btn.next",
+      prevEl: ".swiper-describe-one>.slide-control-btn.prev",
+    },
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    loop: false,
+    centeredSlides: true,
+    breakpoints: {
+      300: {
+        spaceBetween: 8,
+      },
+      640: {
+        spaceBetween: 30,
+      },
+      1048: {
+        spaceBetween: 96,
+      },
+    },
+  })
+  new Swiper(".swiper-describe-two", {
+    modules: [Navigation],
+    navigation: {
+      nextEl: ".swiper-describe-two>.slide-control-btn.next",
+      prevEl: ".swiper-describe-two>.slide-control-btn.prev",
+    },
+    direction: "horizontal",
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    loop: false,
+    centeredSlides: true,
+    breakpoints: {
+      300: {
+        spaceBetween: 8,
+      },
+      640: {
+        spaceBetween: 30,
+      },
+      1048: {
+        spaceBetween: 96,
+      },
+    },
+  })
+  new Swiper(".swiper-describe-three", {
+    modules: [Navigation],
+    navigation: {
+      nextEl: ".swiper-describe-three>.slide-control-btn.next",
+      prevEl: ".swiper-describe-three>.slide-control-btn.prev",
+    },
+    // Optional parameters
+    direction: "horizontal",
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    loop: false,
+    centeredSlides: true,
+    breakpoints: {
+      300: {
+        spaceBetween: 8,
+      },
+      640: {
+        spaceBetween: 30,
+      },
+      1048: {
+        spaceBetween: 96,
       },
     },
   })
