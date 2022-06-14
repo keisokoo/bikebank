3746e8b31ca5ee7224861a04fa17a281d8b618ae
keisokoo <keisokoo@gmail.com>
2022-06-14 23:05:04 +0900

    add modal

---
+++ src/scripts/main.ts
@@ -0,0 +1,53 @@
+function runModal(_id: string) {
+  const modalTarget = document.querySelector(`#modal-${_id}`)
+  if (modalTarget) {
+    modalTarget.removeAttribute("disabled")
+    modalTarget.setAttribute("active", "")
+  } else {
+    closeModal()
+  }
+}
+function closeModal() {
+  const activeModal = document.querySelector(".modal-wrap[active]")
+  if (!activeModal) return
+  activeModal.removeAttribute("active")
+  activeModal.setAttribute("disabled", "")
+  const url = new URL(window.location as any)
+  url.searchParams.delete("modal")
+  window.history.pushState({}, null, url)
+}
+window.addEventListener("popstate", (event) => {
+  const params = new URLSearchParams(
+    (event.currentTarget as Window).location.search
+  )
+  const id = params.get("modal")
+  if (id) {
+    runModal(id)
+  } else {
+    closeModal()
+  }
+})
+document.addEventListener("DOMContentLoaded", async () => {
+  const closeModals = document.querySelectorAll(".close-modal")
+  for (let i = 0; i < closeModals.length; i++) {
+    closeModals[i].addEventListener("click", closeModal)
+  }
+  const modalButton = document.querySelectorAll(
+    ".s2-motors-wrapper img[data-href]"
+  )
+  for (let i = 0; i < modalButton.length; i++) {
+    modalButton[i].addEventListener("click", (e) => {
+      const target = e.currentTarget as HTMLButtonElement
+      const attr = target.getAttribute("data-href")
+      const url = new URL(window.location as any)
+      url.searchParams.set("modal", attr)
+      runModal(attr)
+      window.history.pushState({ modal: attr }, attr, url)
+    })
+  }
+  const params = new URLSearchParams(window.location.search)
+  const id = params.get("modal")
+  if (id) {
+    runModal(id)
+  }
+})
