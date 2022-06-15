267bf105cf40c354a1ad17fb7f41ce450528a96b
keisokoo <keisokoo@doubled.kr>
2022-06-15 12:02:22 +0900

    add, scripts

---
+++ src/scripts/viva.ts
@@ -0,0 +1,28 @@
+import Swiper, { Navigation } from "swiper"
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Swiper(".swiper-describe-one", {
+    // Optional parameters
+    direction: "horizontal",
+    modules: [Navigation],
+    navigation: {
+      nextEl: ".swiper-describe-one>.slide-control-btn.next",
+      prevEl: ".swiper-describe-one>.slide-control-btn.prev",
+    },
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    loop: false,
+    centeredSlides: true,
+    breakpoints: {
+      300: {
+        spaceBetween: 8,
+      },
+      640: {
+        spaceBetween: 30,
+      },
+      1048: {
+        spaceBetween: 96,
+      },
+    },
+  })
+})
